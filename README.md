34d40889e416936a5a423e04226f75cc91dab74c
hamdyaea <hamdy.aea@protonmail.com>
2024-02-23 21:55:48 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# WxtoImg Keplers fix for Linux  
+![Awesome](awesome.svg) [![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black) ![Python](python.png)
+
+
+## What's this software ?  
+
+The purpose of this program is to correct the error that may appear in the wxtoimg program which announces that the Keplers data is too old.  
+  
+## Installation   
+
+You need to have Python3 installed.  
+Clone this repository.  
+  
+  
+## To run the software   
+  
+
+```sh
+sudo python LizardSpock.py
+```  
+
+
+## Developer - Author  
+
+Hamdy Abou El Anein  
+
---
+++ wxcorrector.py
@@ -0,0 +1,33 @@
+# Developer : Hamdy Abou El Anein
+# hamdy.aea@protonmail.com
+
+# TO RUN :
+
+# sudo python wxcorrector.py
+
+import urllib.request
+import os
+
+
+def download_directly(url, destination_path):
+    # Ensure the destination directory exists
+    os.makedirs(os.path.dirname(destination_path), exist_ok=True)
+    print(f"Ensured that the directory {os.path.dirname(destination_path)} exists.")
+
+    # Download the file directly to the destination path
+    with urllib.request.urlopen(url) as response, open(
+        destination_path, "wb"
+    ) as out_file:
+        out_file.write(response.read())
+        print(f"Successfully downloaded the file to {destination_path}.")
+
+
+# URL of the file to download
+url = "https://celestrak.org/NORAD/elements/weather.txt"
+# Destination path (Ensure the directory exists and you have necessary permissions)
+destination_path = "/usr/share/wx/tle/weather.txt"
+
+print("Starting the file download process...")
+# Execute the function
+download_directly(url, destination_path)
+print("File download and copy process completed successfully.")
